3fcb86bc17b6a271f672e77de422d6c26a2c7bcf
Jafar Sadiq <jsadiq369@gmail.com>
2026-09-05 20:24:59 +0530

    added email debus logs for OTP

--- src/main/java/com/Service/EmailService.java
+++ src/main/java/com/Service/EmailService.java
@@ -105,18 +105,17 @@ public class EmailService {
 
             message.setTo(email);
             message.setSubject("Fruit Bazaar OTP Verification");
+
             message.setText(
                     "Your OTP is: " + otp +
                     "\n\nValid for 5 minutes."
             );
-            System.out.println("MAIL USER = " + System.getenv("MAIL_USERNAME"));
+
             mailSender.send(message);
 
             System.out.println("OTP EMAIL SENT SUCCESSFULLY");
 
             return true;
-            
-            
 
         } catch (Exception e) {
 
